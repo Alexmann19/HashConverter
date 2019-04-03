d558e8c8f9c238b70c5cadb344f42cc5328398d9
Alexander Lehr <Alexlehr@hotmail.de>
2019-04-03 14:39:42 +0200

    add programm

---
+++ HashConverter/HashConverter/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace HashConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tbsave = new System.Windows.Forms.TextBox();
+            this.tbload = new System.Windows.Forms.TextBox();
+            this.tbhash = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbtext = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Konvertieren";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbload);
+            this.groupBox1.Controls.Add(this.tbsave);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(227, 322);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(296, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Speichern/Laden";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Speichern";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Laden";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tbsave
+            // 
+            this.tbsave.Location = new System.Drawing.Point(89, 22);
+            this.tbsave.Name = "tbsave";
+            this.tbsave.Size = new System.Drawing.Size(201, 20);
+            this.tbsave.TabIndex = 2;
+            // 
+            // tbload
+            // 
+            this.tbload.Location = new System.Drawing.Point(89, 66);
+            this.tbload.Name = "tbload";
+            this.tbload.Size = new System.Drawing.Size(201, 20);
+            this.tbload.TabIndex = 3;
+            // 
+            // tbhash
+            // 
+            this.tbhash.Location = new System.Drawing.Point(12, 247);
+            this.tbhash.Multiline = true;
+            this.tbhash.Name = "tbhash";
+            this.tbhash.ReadOnly = true;
+            this.tbhash.Size = new System.Drawing.Size(510, 69);
+            this.tbhash.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "HashCode:";
+            // 
+            // tbtext
+            // 
+            this.tbtext.Location = new System.Drawing.Point(13, 13);
+            this.tbtext.Multiline = true;
+            this.tbtext.Name = "tbtext";
+            this.tbtext.Size = new System.Drawing.Size(510, 215);
+            this.tbtext.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 431);
+            this.Controls.Add(this.tbtext);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbhash);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "HashConverter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbload;
+        private System.Windows.Forms.TextBox tbsave;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbhash;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbtext;
+    }
+}
+
---
+++ HashConverter/HashConverter/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace HashConverter
+{
+    public partial class Form1 : Form
+    {
+        public static string convert = "";
+        public static String pfad1 = @"C:\HashConverter\";
+        public static String pfad2 = ".ini";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string GetMD5Hash(string TextToHash)
+        {
+            if ((TextToHash == null) || (TextToHash.Length == 0))
+            {
+                return string.Empty;
+            }
+
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] textToHash = Encoding.Default.GetBytes(TextToHash);
+            byte[] result = md5.ComputeHash(textToHash);
+
+            return System.BitConverter.ToString(result);
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tbtext.Text != "")
+            {
+                convert = GetMD5Hash(tbtext.Text);
+                tbhash.Text = convert;
+            }
+            else
+            MessageBox.Show("Sie haben keinen Text eingegeben!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (tbhash.Text == String.Empty)
+            {
+                MessageBox.Show("Es wurde kein Hashcode genneriert", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            if (tbsave.Text == String.Empty)
+            {
+                MessageBox.Show("Sie haben keinen Dateinamen angegeben", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            if (!Directory.Exists(pfad1))
+            {
+                System.IO.Directory.CreateDirectory(pfad1);
+            }
+            else
+            if (File.Exists(pfad1 + tbsave.Text + pfad2))
+            {
+                File.Delete(pfad1 + tbsave.Text + pfad2);
+            }
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(pfad1 + tbsave.Text + pfad2, true))
+            {
+                file.WriteLine(tbhash.Text);
+            }
+            if (tbsave.Text != String.Empty)
+            {
+                MessageBox.Show("Sie haben den Hashcode erfoldreich gespeichert", "Erfolg", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] eingabe = null;
+            try
+            {
+                // einlesen der Datei in ein String-Array
+                eingabe = File.ReadAllLines(pfad1 + tbload.Text + pfad2, Encoding.UTF8);
+            }
+            catch (Exception ex)
+            {
+                // Datei nicht vorhanden ...
+                MessageBox.Show("Fehler beim Einlesen:\r\n", ex.Message);
+                return;
+            }
+            tbhash.Text = eingabe[0];
+            tbtext.Text = "Ein Md5 Hashcode kann nicht entschlüsselt werden deshalb ist nur der Code geladen worden!";
+            MessageBox.Show("Sie haben den Hashcode erfolgreich geladen", "Laden erfolgreich", MessageBoxButtons.OK);
+        }
+    }
+}
